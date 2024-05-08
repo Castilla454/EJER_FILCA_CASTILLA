b47e3b69131695ae73bef191614755b042df8317
Filca <alexfilcakov@gmail.com>
2024-05-08 09:16:27 +0200

    GestionUsuarios

--- src/GestionUsuarios.java
+++ src/GestionUsuarios.java
@@ -1,2 +1,13 @@
 public class GestionUsuarios {
+    public void registrarUsuario(String nombre, String correo, String contraseña) {
+        System.out.println("Usuario registrado: " + nombre + " - " + correo);
+    }
+
+    public void eliminarUsuario(int id) {
+        System.out.println("Usuario eliminado con ID: " + id);
+    }
+
+    public void autenticarUsuario(String correo, String contraseña) {
+        System.out.println("Usuario autenticado con correo: " + correo);
+    }
 }
